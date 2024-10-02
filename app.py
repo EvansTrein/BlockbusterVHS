33ad66fd62e1c825549617ee8a460d6f96846c54
Evans <evanstrein@icloud.com>
2024-10-02 03:08:16 +0300

    add validation data db and add home page logic

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import render_template, request, redirect, flash
 from config import app, db
 from models import VhsTape, Client, Rental
-from re import fullmatch
+from database_validation import (
+    validateCreateVhs,
+    validateUpdateVhs,
+    validateCreateClient,
+    validateUpdateClient,
+    validateCreateRental,
+)
 
 
 with app.app_context():
@@ -10,7 +16,14 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return render_template("home_page.html")
+    data = {
+        'total_rentals': Rental.query.count(),
+        'total_clients': Client.query.count(),
+        'total_films': VhsTape.query.count(),
+        'total_movies_in_stock': db.session.query(db.func.sum(VhsTape.count)).scalar()
+    }
+
+    return render_template("home_page.html", **data)
 
 
 @app.route("/clear_database", methods=["POST"])
@@ -35,20 +48,20 @@ def all_vhstapes():
     film_title = request.args.get("film_title")
     year_of_release = request.args.get("year_of_release", type=int)
     age_rating = request.args.get("age_rating")
-    data = {'total': total}
+    data = {"total": total}
 
     if film_title:
-        all_vhstapes = VhsTape.query.filter(VhsTape.title.like(f'%{film_title}%')).all()
-        count = VhsTape.query.filter(VhsTape.title.like(f'%{film_title}%')).count()
-        data['count'] = count
+        all_vhstapes = VhsTape.query.filter(VhsTape.title.like(f"%{film_title}%")).all()
+        count = VhsTape.query.filter(VhsTape.title.like(f"%{film_title}%")).count()
+        data["count"] = count
     elif year_of_release:
         all_vhstapes = VhsTape.query.filter(VhsTape.year == year_of_release).all()
         count = VhsTape.query.filter(VhsTape.year == year_of_release).count()
-        data['count'] = count
+        data["count"] = count
     elif age_rating:
         all_vhstapes = VhsTape.query.filter(VhsTape.age_rating == age_rating).all()
         count = VhsTape.query.filter(VhsTape.age_rating == age_rating).count()
-        data['count'] = count
+        data["count"] = count
     else:
         all_vhstapes = VhsTape.query.order_by(VhsTape.year.desc()).all()
 
@@ -58,33 +71,31 @@ def all_vhstapes():
 @app.route("/create_vhstape", methods=["GET", "POST"])
 def create_vhstape():
     if request.method == "POST":
-        title = request.form["title"]
-        year = request.form["year"]
-        age_rating = request.form["age_rating"]
-        count = request.form["count"]
+        data_in = {
+            "key_in": "create",
+            "title": request.form["title"],
+            "year": request.form["year"],
+            "age_rating": request.form["age_rating"],
+            "count": request.form["count"],
+        }
 
-        # checking the conditions for a database record
-        if VhsTape.query.filter_by(title=title).first():
-            flash("There's already a tape like this")
+        valid_res = validateCreateVhs(data_in)
+        if valid_res["error"]:
+            flash(valid_res["error_text"])
             return render_template("create_vhstape_page.html")
-        elif not all((title, year, age_rating, count)):
-            flash("All fields must be filled in")
-            return render_template("create_vhstape_page.html")
-        elif len(title) > 100 or len(age_rating) > 10:
-            flash(
-                'The number of characters in the "name film" or "age rating" field is exceeded'
+
+        try:
+            vhs = VhsTape(
+                title=data_in["title"],
+                year=data_in["year"],
+                age_rating=data_in["age_rating"],
+                count=data_in["count"],
             )
-            return render_template("create_vhstape_page.html")
-        else:
-            try:
-                vhs = VhsTape(
-                    title=title, year=year, age_rating=age_rating, count=count
-                )
-                db.session.add(vhs)
-                db.session.commit()
-                return redirect("/create_vhstape")
-            except Exception as err:
-                return f"There was an issue adding your VHS tape >>>> {str(err)}"
+            db.session.add(vhs)
+            db.session.commit()
+            return redirect("/create_vhstape")
+        except Exception as err:
+            return f"There was an issue adding your VHS tape >>>> {str(err)}"
 
     else:
         return render_template("create_vhstape_page.html")
@@ -94,26 +105,25 @@ def create_vhstape():
 def update_vhstape(id):
     vhs = VhsTape.query.get(id)
     if request.method == "POST":
-        title = request.form["title"]
-        year = request.form["year"]
-        age_rating = request.form["age_rating"]
-        count = int(request.form["count"])
+        data_in = {
+            "key_in": "update",
+            "obj_bd": vhs,
+            "title": vhs.title,
+            "year": request.form["year"],
+            "age_rating": request.form["age_rating"],
+            "count": request.form["count"],
+        }
 
-        if count < vhs.issued_to_clients:
-            flash("The total quantity may not be less than the sum of available and issued")
-            return redirect(f"/vhs/{id}/update")
-
-        if all((title, year, age_rating, count)):
-            vhs.title = title
-            vhs.year = year
-            vhs.age_rating = age_rating
-            vhs.count = count
-            vhs.available_quantity = count - vhs.issued_to_clients
-        else:
-            flash("Fields cannot be empty")
+        valid_res = validateUpdateVhs(data_in)
+        if valid_res:
+            flash(valid_res)
             return redirect(f"/vhs/{id}/update")
 
         try:
+            vhs.year = data_in["year"]
+            vhs.age_rating = data_in["age_rating"]
+            vhs.count = data_in["count"]
+            vhs.available_quantity = int(data_in["count"]) - vhs.issued_to_clients
             db.session.commit()
             return redirect(f"/vhs/{id}")
         except Exception as err:
@@ -140,41 +150,34 @@ def delete_vhstape(id):
         return f"There was a problem deleting that VHS tape >>>> {str(err)}"
 
 
-
 # ===========================================================================================================
 
 
 @app.route("/create_client", methods=["GET", "POST"])
 def create_client():
     if request.method == "POST":
-        name = request.form["name"]
-        age = request.form["age"]
-        phone = request.form["phone"]
+        data_in = {
+            "key_in": "create",
+            "name": request.form["name"],
+            "age": request.form["age"],
+            "phone": request.form["phone"],
+        }
 
-        # checking the conditions for a database record
-        if Client.query.filter_by(name=name).first():
-            flash("There's already a tape like this")
+        valid_res = validateCreateClient(data_in)
+        if valid_res["error"]:
+            flash(valid_res["error_text"])
             return render_template("create_client_page.html")
-        elif not all((name, age, phone)):
-            flash("All fields must be filled in")
-            return render_template("create_client_page.html")
-        elif int(age) < 14:
-            flash("The client cannot be under 14 years of age")
-            return render_template("create_client_page.html")
-        elif not fullmatch(r"\+\d{10,20}", phone):
-            flash(
-                'Incorrect phone number, the number starts with "+country code" and has 10 to 20 digits'
+
+        try:
+            new_client = Client(
+                name=data_in["name"], age=data_in["age"], phone=data_in["phone"]
             )
-            return render_template("create_client_page.html")
-        else:
-            try:
-                new_client = Client(name=name, age=age, phone=phone)
-                db.session.bind = "clients"
-                db.session.add(new_client)
-                db.session.commit()
-                return redirect("/create_client")
-            except Exception as err:
-                return f"There was an issue adding your client >>>> {str(err)}"
+            db.session.bind = "clients"
+            db.session.add(new_client)
+            db.session.commit()
+            return redirect("/create_client")
+        except Exception as err:
+            return f"There was an issue adding your client >>>> {str(err)}"
 
     else:
         return render_template("create_client_page.html")
@@ -185,20 +188,24 @@ def all_clients():
     total = Client.query.count()
     min_age = request.args.get("min_age", type=int)
     max_age = request.args.get("max_age", type=int)
-    data = {'total': total}
+    data = {"total": total}
 
     if min_age and max_age:
         all_clients = Client.query.filter(Client.age.between(min_age, max_age)).all()
         count = Client.query.filter(Client.age.between(min_age, max_age)).count()
-        data['count'] = count
+        data["count"] = count
     elif min_age:
-        all_clients = Client.query.filter(Client.age >= min_age).order_by(Client.age.asc()).all()
+        all_clients = (
+            Client.query.filter(Client.age >= min_age).order_by(Client.age.asc()).all()
+        )
         count = Client.query.filter(Client.age >= min_age).count()
-        data['count'] = count
+        data["count"] = count
     elif max_age:
-        all_clients = Client.query.filter(Client.age <= max_age).order_by(Client.age.asc()).all()
+        all_clients = (
+            Client.query.filter(Client.age <= max_age).order_by(Client.age.asc()).all()
+        )
         count = Client.query.filter(Client.age <= max_age).count()
-        data['count'] = count
+        data["count"] = count
     else:
         all_clients = Client.query.order_by(Client.name.desc()).all()
 
@@ -211,37 +218,49 @@ def client(id):
     all_clients_films = Rental.query.filter(Rental.client_name == client.name).all()
     count_films = Rental.query.filter(Rental.client_name == client.name).count()
 
-    # ПЕРЕДЕЛАТЬ ПРОВЕРКУ!!!
     if request.method == "POST":
         fiml_id = int(request.form["fiml_id"])
+        total_rentals = Rental.query.count()
 
-        if VhsTape.query.filter_by(id_num=fiml_id).first():
-            create_rental(id, fiml_id)
-        else:
+        if not VhsTape.query.filter_by(id_num=fiml_id).first():
             flash("A movie with that ID does not exist")
+        else:
+            create_rental(id, fiml_id)
+            if total_rentals == Rental.query.count():
+                flash(
+                    "Failed to issue the movie to the client, check if the ID is correct or if the movie has not been issued previously"
+                )
 
         return redirect(f"/client/{id}")
     else:
-        return render_template("client_page.html", client=client, all_clients_films=all_clients_films, count_films=count_films)
+        return render_template(
+            "client_page.html",
+            client=client,
+            all_clients_films=all_clients_films,
+            count_films=count_films,
+        )
 
 
 @app.route("/client/<int:id>/update", methods=["GET", "POST"])
 def update_client(id):
     client = Client.query.get(id)
     if request.method == "POST":
-        name = request.form["name"]
-        age = request.form["age"]
-        phone = request.form["phone"]
+        data_in = {
+            "key_in": "update",
+            "obj_bd": client,
+            "name": client.name,
+            "age": request.form["age"],
+            "phone": request.form["phone"],
+        }
 
-        if all((name, age, phone)):
-            client.name = name
-            client.age = age
-            client.phone = phone
-        else:
-            flash("fields cannot be empty")
+        valid_res = validateUpdateClient(data_in)
+        if valid_res:
+            flash(valid_res)
             return redirect(f"/client/{id}/update")
 
         try:
+            client.age = data_in["age"]
+            client.phone = data_in["phone"]
             db.session.commit()
             return redirect(f"/client/{id}")
         except Exception as err:
@@ -276,16 +295,18 @@ def all_rentals():
     total = Rental.query.count()
     client_name = request.args.get("client_name")
     film_title = request.args.get("film_title")
-    data = {'total': total}
+    data = {"total": total}
 
     if client_name:
-        all_issued = Rental.query.filter(Rental.client_name.like(f"%{client_name}%")).all()
+        all_issued = Rental.query.filter(
+            Rental.client_name.like(f"%{client_name}%")
+        ).all()
         count = Rental.query.filter(Rental.client_name == client_name).count()
-        data['count'] = count
+        data["count"] = count
     elif film_title:
         all_issued = Rental.query.filter(Rental.title_vhs.like(f"%{film_title}%")).all()
         count = Rental.query.filter(Rental.title_vhs == film_title).count()
-        data['count'] = count
+        data["count"] = count
     else:
         all_issued = Rental.query.all()
 
@@ -295,46 +316,38 @@ def all_rentals():
 @app.route("/create_rental", methods=["GET", "POST"])
 def create_rental(*args):
     if request.method == "POST":
-        client_id = request.form["client_id"] if len(args) == 0 else args[0]
-        vhs_tape_id = request.form["vhs_tape_id"] if len(args) == 0 else args[1]
-        client = Client.query.get(client_id)
-        vhs = VhsTape.query.get(vhs_tape_id)
-        existing_rental = Rental.query.filter_by(title_vhs=vhs.title, client_name=client.name).first()
+        data_in = {
+            "client_id": request.form["client_id"] if len(args) == 0 else args[0],
+            "vhs_tape_id": request.form["vhs_tape_id"] if len(args) == 0 else args[1],
+        }
 
-        if (client is None) and (vhs is None):
-            flash("The movie and client with those ID's do not exist")
+        valid_res = validateCreateRental(data_in)
+        if valid_res["error"]:
+            flash(valid_res["error_text"])
             return render_template("create_rental_page.html")
-        elif not client:
-            flash("Client with this ID does not exist")
-            return render_template("create_rental_page.html")
-        elif not vhs:
-            flash("A movie with that ID does not exist")
-            return render_template("create_rental_page.html")
-        elif existing_rental:
-            flash("This customer has already rented this movie")
-            return render_template("create_rental_page.html")
-        elif vhs.available_quantity == 0:
-            flash("Out of stock")
-            return render_template("create_rental_page.html")
-        else:
-            try:
-                rental = Rental(client_id=client_id, vhs_tape_id=vhs_tape_id)
-                db.session.bind = "rentals"
-                db.session.add(rental)
-                vhs.issued_to_clients += 1
-                vhs.available_quantity -= 1
-                db.session.commit()
-                return redirect("/create_rental")
-            except Exception as err:
-                return f"There was a problem with the cassette issue >>>> {str(err)}"
-    
+
+        try:
+            vhs = VhsTape.query.get(data_in["vhs_tape_id"])
+            rental = Rental(
+                client_id=data_in["client_id"], vhs_tape_id=data_in["vhs_tape_id"]
+            )
+            db.session.bind = "rentals"
+            db.session.add(rental)
+            vhs.issued_to_clients += 1
+            vhs.available_quantity -= 1
+            db.session.commit()
+            return redirect("/create_rental")
+        except Exception as err:
+            return f"There was a problem with the cassette issue >>>> {str(err)}"
+
     else:
         return render_template("create_rental_page.html")
-    
+
 
 @app.route("/rental/<int:id>/delete")
 def delete_rental(id):
     rental = Rental.query.get_or_404(id)
+    referrer_url = request.referrer
 
     try:
         vhs = VhsTape.query.get(rental.vhs_tape_id)
@@ -342,6 +355,8 @@ def delete_rental(id):
         vhs.available_quantity += 1
         db.session.delete(rental)
         db.session.commit()
+        if "client" in referrer_url:
+            return redirect(referrer_url)
         return redirect("/all_rentals")
     except Exception as err:
         return f"There was a problem with deleting this lease >>>> {str(err)}"
---
+++ database_validation.py
@@ -0,0 +1,128 @@
+from models import VhsTape, Client, Rental
+from re import fullmatch
+
+
+def validateCreateVhs(data_in: dict) -> dict:
+    answer = {"error": False, "error_text": ""}
+    title = data_in["title"]
+    year = data_in["year"]
+    age_rating = data_in["age_rating"]
+    count = data_in["count"]
+    key_in = data_in["key_in"]
+
+    if VhsTape.query.filter_by(title=title).first() and key_in == "create":
+        answer["error"] = True
+        answer["error_text"] = "There's already a movie like that"
+        return answer
+    elif not all((title, year, age_rating, count)):
+        answer["error"] = True
+        answer["error_text"] = "All fields must be filled in"
+        return answer
+    elif not 1900 <= int(data_in["year"]) <= 10000:
+        answer["error"] = True
+        answer["error_text"] = "Incorrect year format"
+        return answer
+    elif len(title) > 100 or len(age_rating) > 10:
+        answer["error"] = True
+        answer["error_text"] = (
+            'The number of characters in the "name film" or "age rating" field is exceeded'
+        )
+        return answer
+    else:
+        return answer
+
+
+def validateUpdateVhs(data_in: dict) -> str:
+    error_text = ""
+    obj_bd = data_in["obj_bd"]
+    count = data_in["count"]
+
+    is_vaild = validateCreateVhs(data_in)
+
+    if not count.isdigit():
+        error_text = "count can only be an integer"
+        return error_text
+    if int(count) < obj_bd.issued_to_clients:
+        error_text = (
+            "The total quantity may not be less than the sum of available and issued"
+        )
+        return error_text
+    elif is_vaild["error"]:
+        return is_vaild["error_text"]
+    else:
+        return error_text
+
+
+def validateCreateClient(data_in: dict) -> dict:
+    answer = {"error": False, "error_text": ""}
+    name = data_in["name"]
+    age = data_in["age"]
+    phone = data_in["phone"]
+    key_in = data_in["key_in"]
+
+    if Client.query.filter_by(name=name).first() and key_in == "create":
+        answer["error"] = True
+        answer["error_text"] = "There's already a client like that"
+        return answer
+    elif not all((name, age, phone)):
+        answer["error"] = True
+        answer["error_text"] = "All fields must be filled in"
+        return answer
+    elif int(age) < 14:
+        answer["error"] = True
+        answer["error_text"] = "The client cannot be under 14 years of age"
+        return answer
+    elif not fullmatch(r"\+\d{10,20}", phone):
+        answer["error"] = True
+        answer["error_text"] = (
+            "incorrect phone number (format: +country code... from 10 to 20 digits)"
+        )
+        return answer
+    else:
+        return answer
+
+
+def validateUpdateClient(data_in: dict) -> str:
+    error_text = ""
+
+    is_vaild = validateCreateClient(data_in)
+
+    if is_vaild["error"]:
+        return is_vaild["error_text"]
+    else:
+        return error_text
+
+
+def validateCreateRental(data_in: dict) -> dict:
+    answer = {"error": False, "error_text": ""}
+
+    client_id = data_in["client_id"]
+    vhs_tape_id = data_in["vhs_tape_id"]
+    client = Client.query.get(client_id)
+    vhs = VhsTape.query.get(vhs_tape_id)
+    existing_rental = Rental.query.filter_by(
+        title_vhs=vhs.title, client_name=client.name
+    ).first()
+
+    if (client is None) and (vhs is None):
+        answer["error"] = True
+        answer["error_text"] = "The movie and client with those ID's do not exist"
+        return answer
+    elif not client:
+        answer["error"] = True
+        answer["error_text"] = "Client with this ID does not exist"
+        return answer
+    elif not vhs:
+        answer["error"] = True
+        answer["error_text"] = "A movie with that ID does not exist"
+        return answer
+    elif existing_rental:
+        answer["error"] = True
+        answer["error_text"] = "This customer has already rented this movie"
+        return answer
+    elif vhs.available_quantity == 0:
+        answer["error"] = True
+        answer["error_text"] = "Out of stock"
+        return answer
+    else:
+        return answer
