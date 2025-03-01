8aebd643e7939b58c0f833520f993ada47150553
Evans <evanstrein@icloud.com>
2025-03-01 15:56:36 +0300

    add font

--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -1,5 +1,6 @@
 <template>
-  <h1>Home</h1>
+  <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam eveniet praesentium cumque vero fugit molestias. Corporis commodi aliquam veritatis id nihil laborum fuga dolor nemo consequuntur ipsam. Quam ipsam minus explicabo, quibusdam perspiciatis ut id aspernatur praesentium voluptate quo placeat libero quod dolore et error aliquid enim, quis saepe consequuntur.</h1>
+  <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam eveniet praesentium cumque vero fugit molestias. Corporis commodi aliquam veritatis id nihil laborum fuga dolor nemo consequuntur ipsam. Quam ipsam minus explicabo, quibusdam perspiciatis ut id aspernatur praesentium voluptate quo placeat libero quod dolore et error aliquid enim, quis saepe consequuntur.</h1>
 </template>
 
 <script setup lang="ts"></script>
